0086497a8d3b8021f46f33e42d639abc1ed590f7
Florent Messa <florent.messa@gmail.com>
2018-07-13 15:15:28 +0200

    fix: imports

--- discussions/models/base.py
+++ discussions/models/base.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 
